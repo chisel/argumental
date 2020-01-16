153dd9a58b793752fc624e1e735a68caba49a525
Ramtin Soltani <ramtinsoltani93@gmail.com>
2020-01-16 01:33:38 -0800

    Changed importing command modules position and added a note about app context after importing modules.

--- examples/dog/src/dog.ts
+++ examples/dog/src/dog.ts
@@ -2,12 +2,20 @@
 import path from 'path';
 import app from '../../..';
 
+app
+// Set version from package.json
+.version(require(path.resolve(__dirname, '..', 'package.json')).version);
+
+// Importing command modules
 import './common';
 import './commands/list';
 import './commands/search';
 import './commands/image';
 
-app
-// Set version from package.json
-.version(require(path.resolve(__dirname, '..', 'package.json')).version)
-.parse(process.argv);
+/** NOTE:
+* Accessing app at this line will use the last defined command as context unless
+* `command()`, `global`, or `top` is called on app.
+* Read about chaining and context here: https://github.com/chisel/argumental#chaining-and-context
+*/
+
+app.parse(process.argv);
